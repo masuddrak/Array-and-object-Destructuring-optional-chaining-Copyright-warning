f475d03975ed57bcc0be36479bde0852a1bebad3
masud rana <masudranaacb@gmail.com>
2022-03-09 13:11:12 -0800

    norlam arrauy distructuring

---
+++ app.js
@@ -0,0 +1,9 @@
+// Array and object Destructuring, optional chaining
+
+const numbers=[7,5,54]
+ const [x,y,z]=numbers
+//  console.log(x,y,z)
+const names=(frist,last)=>{
+    const fulllName=[frist,last]
+    return fulllName
+}
