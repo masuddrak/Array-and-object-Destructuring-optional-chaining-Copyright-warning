2a61c3bdd4aaa12914c3c2af576e99e9945abf2e
masud rana <masudranaacb@gmail.com>
2022-03-09 13:27:48 -0800

    object distructuring

--- app.js
+++ app.js
@@ -10,3 +10,17 @@ const names=(frist,last)=>{
 
 const [frist,last]=names('Masud','Rana')
 console.log(frist,last)
+
+//  object Destructuring
+ const employe={
+     name:'Masud Rana',
+     Ide:'vs cod',
+     languages:['html','javascript','node.js'],
+     localJob:{
+         name:'defodil university',
+         position:'second',
+         duration:'5 years'
+     }
+ }
+ const {duration}=employe.localJob
+ console.log(duration)
